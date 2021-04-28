261cc0b201dfef2f4a6d3674b21d59cba8af4a10
Mohammad Fahim Abrar <fahimabrar@appscode.com>
2021-04-28 12:54:02 +0600

    Add pause/resume BackupConfiguration
    Signed-off-by: Mohammad Fahim Abrar <fahimabrar@appscode.com>

---
+++ pkg/resumer/util.go
@@ -0,0 +1,38 @@
+package resumer
+
+import (
+	"context"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"stash.appscode.dev/apimachinery/apis"
+	stash "stash.appscode.dev/apimachinery/apis/stash/v1beta1"
+	scs "stash.appscode.dev/apimachinery/client/clientset/versioned/typed/stash/v1beta1"
+	scsutil "stash.appscode.dev/apimachinery/client/clientset/versioned/typed/stash/v1beta1/util"
+)
+
+func ResumeBackupConfiguration(stashClient scs.StashV1beta1Interface, dbMeta metav1.ObjectMeta) error {
+	configs, err := stashClient.BackupConfigurations(dbMeta.Namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	var dbBackupConfig *stash.BackupConfiguration
+	for _, config := range configs.Items {
+		if config.Spec.Target.Ref.Name == dbMeta.Name && config.Spec.Target.Ref.Kind == apis.KindAppBinding {
+			dbBackupConfig = &config
+			break
+		}
+	}
+
+	if dbBackupConfig != nil {
+		_, err := scsutil.TryUpdateBackupConfiguration(context.TODO(), stashClient, dbBackupConfig.ObjectMeta, func(configuration *stash.BackupConfiguration) *stash.BackupConfiguration {
+			configuration.Spec.Paused = false
+			return configuration
+		}, metav1.UpdateOptions{})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
